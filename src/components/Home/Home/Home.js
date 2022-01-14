3d47eb1cb7b942a9327ac8d4e5bdebba6133cbb1
Rakibul santo <SANTODON176@gmail.com>
2022-01-14 23:32:28 +0600

    add features

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import FeaturesProducts from '../FeaturesProducts/FeaturesProducts';
 
 
 const Home = () => {
   return (
     <div>
       <h1>Home </h1>
-     
+     <FeaturesProducts/>
     </div>
   );
 };
