66cf711697e25e51ebcdb2db6ffd9a2c1ee3029c
Rakibul santo <SANTODON176@gmail.com>
2022-01-14 23:03:00 +0600

    ad

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import Banner from '../Banner/Banner';
-import BestSelling from '../BestSelling/BestSelling';
-import FeaturesProducts from '../FeaturesProducts/FeaturesProducts';
-import SingleSlider from '../SingleSlider/SingleSlider';
-import Blog from '../Blog/Blog';
-import MultipleSlider from '../MultipleSlider/MultipleSlider';
-import Brand from '../Brand/Brand';
-import FruitBanner from '../FruitBanner/FruitBanner';
-import Reviews from '../Reviews/Reviews';
+
 
 const Home = () => {
   return (
     <div>
       <h1>Home </h1>
-      <Banner/>
-      <SingleSlider/>
-      <FruitBanner/>
-      <FeaturesProducts/>
-      <BestSelling/>
-      <Blog/>
-      <MultipleSlider/>
-      <Reviews/>
-      <Brand/>
+     
     </div>
   );
 };
