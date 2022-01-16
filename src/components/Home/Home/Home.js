895f2b82b5d3ae6d1b81ce8c45919d922487d6f9
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-16 23:26:56 +0600

    banner added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,14 +2,14 @@ import { Box } from "@mui/material";
 import React from "react";
 import Navigation from "../../Shared/Navigation/Navigation";
 import TopHeader from "../../Shared/TopHeader/TopHeader";
-import Banner from "../Banner/Banner";
+import BannerAndSearch from "../BannerAndSearch/BannerAndSearch";
 
 const Home = () => {
   return (
     <Box>
       <TopHeader />
       <Navigation />
-      <Banner />
+      <BannerAndSearch />
     </Box>
   );
 };
