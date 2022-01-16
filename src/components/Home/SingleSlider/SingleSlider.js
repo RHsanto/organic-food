deefd2ca85480e7221058a560f8b44aa87490149
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-16 18:54:18 +0600

    single slider with json data added

--- src/components/Home/SingleSlider/SingleSlider.js
+++ src/components/Home/SingleSlider/SingleSlider.js
@@ -1,23 +1,27 @@
 import { Box, Container, Typography } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import on1 from "../../../images/single/1.webp";
-import on2 from "../../../images/single/2.webp";
-import on3 from "../../../images/single/3.webp";
-import on4 from "../../../images/single/4.webp";
-import on5 from "../../../images/single/cat-3.jpg.webp";
 import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
-import './SingleSlider.css'
+import "./SingleSlider.css";
 
 const SingleSlider = () => {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    fetch("/single.json")
+      .then((res) => res.json())
+      .then((data) => setDatas(data));
+  }, []);
+
   function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
       <div onClick={onClick} className={className}>
-      <ArrowForwardIos style={{color:'black', fontSize:'15px'}}></ArrowForwardIos>
-      
+        <ArrowForwardIos
+          style={{ color: "#1c1c1c", fontSize: "14px" }}
+        ></ArrowForwardIos>
       </div>
     );
   }
@@ -26,7 +30,9 @@ const SingleSlider = () => {
     const { className, style, onClick } = props;
     return (
       <div className={className} onClick={onClick}>
-    <ArrowBackIos style={{color:'black', fontSize:'15px'}}></ArrowBackIos>
+        <ArrowBackIos
+          style={{ color: "#1c1c1c", fontSize: "14px" }}
+        ></ArrowBackIos>
       </div>
     );
   }
@@ -36,41 +42,24 @@ const SingleSlider = () => {
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplaySpeed: 1000,
-    autoplay: true,
+    // autoplay: true,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
 
   return (
     <Container>
-    
+      {/* */}
 
       <Slider {...settings}>
-        <Box>
-        <Box sx={{ textAlign: "center" }}>
-          <img style={{display:'inline-block'}} src={on1} alt="" />
-        </Box>
-        </Box>
-        <Box>
-        <Box sx={{ textAlign: "center" }}>
-          <img style={{display:'inline-block'}} src={on2} alt="" />
-        </Box>
-        </Box>
-        <Box>
-        <Box sx={{ textAlign: "center" }}>
-          <img style={{display:'inline-block'}} src={on3} alt="" />
-        </Box>
-        </Box>
-        <Box>
-        <Box sx={{ textAlign: "center" }}>
-          <img style={{display:'inline-block'}} src={on4} alt="" />
-        </Box>
-        </Box>
-        <Box>
-        <Box sx={{ textAlign: "center" }}>
-          <img style={{display:'inline-block'}} src={on5} alt="" />
-        </Box>
-        </Box>
+        {datas.map((data) => (
+          <Box>
+            <Box sx={{ textTransform:'uppercase',fontWeight:'900',textAlign: "center",position:'relative' }}>
+              <img style={{ display: "inline-block" }} src={data.img} alt="" />
+              <Typography sx={{bottom:"20px",left:'17%',position:'absolute',zIndex:'10',textAlign:'center',backgroundColor:"white",padding:'5px 30px'}} variant='h6'>{data.name}</Typography>
+            </Box>
+          </Box>
+        ))}
       </Slider>
     </Container>
   );
