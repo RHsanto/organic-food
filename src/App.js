20bea5973a0caed565b4be910e8bb274ae9d8660
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-18 23:30:45 +0600

    redux initial steps added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home/Home/Home";
 
-
 function App() {
   return (
     <Box className="App">
-
       <Home />
     </Box>
   );
---
+++ src/Redux/Slices/blogSlice.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// thunk to get data using api here
+export const fetchBlogs = createAsyncThunk("blog/fetchBlogs", async () => {
+  console.log("working well");
+  const response = await fetch("/blogs.JSON").then((res) => res.json());
+  return response;
+});
+
+const blogSlice = createSlice({
+  name: "blogs",
+  initialState: {
+    blogs: [],
+    status: "idle",
+  },
+
+  //   main reducers here
+  reducers: {},
+  //   extra reducers for api and other fatching operations
+  extraReducers: (builder) => {
+    builder.addCase(fetchBlogs.fulfilled, (state, action) => {
+      state.blogs = action.payload;
+      state.status = "success";
+    });
+    builder.addCase(fetchBlogs.pending, (state, action) => {
+      state.status = "pending";
+    });
+  },
+});
+
+export default blogSlice.reducer;
