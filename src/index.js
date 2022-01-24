dc0b4a4ab139023c68b918bf7b91b4451fbe23a2
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-24 16:46:03 +0600

    initial setup added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
+import React from "react";
+import ReactDOM from "react-dom";
 import { store } from "./Redux/store";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import * as serviceWorker from "./serviceWorker";
+import { PersistGate } from "redux-persist/integration/react";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
