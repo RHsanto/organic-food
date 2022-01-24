0e1cbad0a0a6a708d56f40efb891dbb1be706a83
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-25 01:27:41 +0600

    redux persist added succesfully

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import blogSlice from "./Slices/blogSlice";
 import {
   persistStore,
@@ -23,11 +23,13 @@ const persistedReducer = persistReducer(persistConfig, blogSlice);
 export const store = configureStore({
   reducer: {
     blogs: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-import { store } from "./Redux/store";
+import { persistor, store } from "./Redux/store";
 import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
 import * as serviceWorker from "./serviceWorker";
 import { PersistGate } from "redux-persist/integration/react";
-
-let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
